5d88a9dffabd6cff2948f687869ed41452bd9a29
Maxime Alay-Eddine <maxime.alayeddine@gmail.com>
2015-01-14 01:25:45 +0100

    Class improvement to init Connections

--- app/models/Tinder.rb
+++ app/models/Tinder.rb
@@ -1,5 +1,8 @@
 class Tinder
 
+  attr_reader :tinder_token
+  attr_reader :connection
+
   def self.connect
     conn = Faraday.new(:url => 'https://api.gotinder.com') do |faraday|
       faraday.request :json             # form-encode POST params
@@ -10,6 +13,16 @@ class Tinder
     # We now must provide the same User-Agent as the iPhone
     conn.headers['User-Agent'] = "Tinder/4.0.9 (iPhone; iOS 8.1.1; Scale/2.00)"
     return conn
+  end
+
+  def initialize(fb_id, fb_token, tinder_token)
+    @facebook_id = fb_id
+    @facebook_token = fb_token
+    @tinder_token = tinder_token
+    conn = Tinder.connect
+    conn.token_auth(tinder_token)
+    conn.headers['X-Auth-Token'] = token
+    @connection = conn
   end
 
 end
